786528b8fe69783da35ef07c0e2d2eece9a0a289
abryantr <54371404+abryantr@users.noreply.github.com>
2022-05-07 09:38:30 -0400

    added CSV function, other stuff

--- src/main.java
+++ src/main.java
@@ -1,10 +1,19 @@
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.io.PrintWriter;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+
+import com.opencsv.CSVWriter;
 
 public class main {
 
@@ -16,7 +25,17 @@ public class main {
 	 * URL
 	 */
 	
-	static String[] desks = new String[] {"campus","city-county","state","sports","culture","opinion","elevate","coronavirus"};
+	//Map which contains the names of each desk and the corresponding URL part needed to access each
+	static Map<String, String> desks = Stream.of(new String[][] {
+		{"campus","University"},
+		{"city-county","City"},
+		{"state","State"},
+		{"sports","Sports"},
+		{"culture","Arts and Culture"},
+		{"opinion","Opinion"},
+		{"elevate","Elevate"},
+		{"coronavirus","COVID-19"}
+	}).collect(Collectors.toMap(data -> data[0], data -> data[1]));
 	
 	/* Format of data internal arrays:
 	 * [0] Title
@@ -30,22 +49,21 @@ public class main {
 	
 	public static void main(String[] args) {
 
-		boolean running = true;
+		//boolean running = true;
 		int page_number = 0;
 		int article_number = 1;
-		while(running) {
+		for(int k = 0; k < 100; k++) {
 			page_number++;
-			Document page_doc = load(desks[0],page_number);
+			Document page_doc = load(desks.get("campus"),page_number);
 			Elements articles = page_doc.getElementsByTag("article");
 			if(articles.size() == 1) {
-				running = false;
+				//running = false;
 				break;
 			}
 
 			//selects second container-fluid, first container-fluid holds four featured articles which also appear in the second container-fluid
 			Elements e = page_doc.getElementsByClass("container-fluid");
 			Element page = e.last();
-			ArrayList<String[]> page_data = new ArrayList<String[]>();
 
 
 			Elements headlines = page.getElementsByClass("headline");
@@ -54,24 +72,13 @@ public class main {
 			int len = headlines.indexOf(headlines.last()) + 1;
 
 			for(int i = 0; i < len; i++) {
-				String[] temp = new String[6];
-				temp[0] = headlines.get(i).text();
-				temp[1] = datelines.get(i).getElementsByTag("a").text();
-				temp[2] = "University";
-				temp[5] = headlines.get(i).getElementsByTag("a").attr("href");
-				temp[4] = getWordCount(temp[5]);
-				temp[3] = temp[5].substring(37, 44);
-				
 				System.out.println(article_number);
 				article_number++;
-
-//				for(int j = 0; j < 6; j++) {
-//					System.out.println(temp[j]);
-//				}
-//				System.out.println();
-				page_data.add(temp);
+				data.add(getData(headlines,datelines,i));
 			}
 		}
+		
+		generateCSV();
 	}
 	
 	public static Document load(String desk, int page) {
@@ -81,6 +88,18 @@ public class main {
 		catch(IOException e) {}
 		
 		return doc;
+	}
+	
+	public static String[] getData(Elements headlines, Elements datelines, int i) {
+		String[] output = new String[5];
+		output[0] = headlines.get(i).text();
+		output[1] = datelines.get(i).getElementsByTag("a").text();
+		output[2] = "University";
+		output[4] = headlines.get(i).getElementsByTag("a").attr("href");
+		//reads the article's publication month and year from the URL
+		output[3] = output[4].substring(37, 44);
+		
+		return output;
 	}
 	
 	public static String getWordCount(String link) {
@@ -95,5 +114,29 @@ public class main {
 		if(str.isEmpty()) return "0";
 		
 		return String.valueOf(str.split("\\s+").length-25);
+	}
+	
+	public static void generateCSV() {
+		File output = new File("output.csv");
+		
+		try {
+			FileWriter writer_f = new FileWriter(output);
+			CSVWriter writer = new CSVWriter(writer_f);
+			writer.writeAll(data);
+			writer.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return;
+	}
+	
+	private static class Counter extends Thread{
+		Counter() {
+			
+		}
+		void main(String[] args) {
+			Counter thread = new Counter();
+		}
 	}
 }
